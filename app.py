5249ebf208a748a3f1fb244063a1ba1a63d725fb
Darth Vader <danieltarancon@gmail.com>
2022-08-26 20:28:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for, request
+from flask import render_template, url_for, request
+
+from models import db, Portfolio, app
 
 
 
@@ -30,6 +32,7 @@ def delete():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
 
 
