8ae28ad06f38f3ad565912da0ecce111b46355ee
Darth Vader <danieltarancon@gmail.com>
2022-08-26 20:28:02 +0200

    Update models.py

--- models.py
+++ models.py
@@ -9,10 +9,23 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
 db = SQLAlchemy(app)
 
-class portfolio(db.Model):
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column('Title', db.String())
+    description = db.Column('Description', db.Text())
+    skills_practiced = db.Column('Skills Practiced', db.Text())
+    github_link = db.Column('Github Link', db.String())
+
+
+
+class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created = db.Column('Created', db.DateTime, default=datetime.datetime.now)
     name = db.Column('Name', db.String())
-    
+
+    def __repr__(self):
+        return f'''< Portfolio (Name: {self.name})'''
+
 
 
