d47b8936497af44b6b86efd0e4309ab065227d8f
Darth Vader <danieltarancon@gmail.com>
2022-08-29 16:43:17 +0200

    Update models.py

--- models.py
+++ models.py
@@ -9,12 +9,13 @@ db = SQLAlchemy(app)
 
 
 
-class Old_Project(db.Model):
+class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column('Title', db.String())
+    date = db.Column('Created', db.DateTime, default=datetime.datetime.now)
     description = db.Column('Description', db.Text())
-    skills_practiced = db.Column('Skills Practiced', db.Text())
-    github_link = db.Column('Github Link', db.String())
+    skills = db.Column('Skills Practiced', db.Text())
+    github = db.Column('Github Link', db.String())
     main_app = db.Column('Main App', db.Text())
     extra_file01 = db.Column('Extra File 01', db.Text())
     extra_file02 = db.Column('Extra File 02', db.Text())
@@ -22,9 +23,10 @@ class Old_Project(db.Model):
 
     def __repr__(self):
         return f'''< Old_Project (Title: {self.title}
+        Created: {self.date}
         Description: {self.description}
-        Skills Practiced: {self.skills_practiced}
-        Github Link: {self.github_link}
+        Skills Practiced: {self.skills}
+        Github Link: {self.github}
         Main App: {self.main_app}
         Extra File 01: {self.extra_file01}
         Extra File 02: {self.extra_file02}
@@ -32,17 +34,6 @@ class Old_Project(db.Model):
 
 
 
-class New_Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column('Title', db.String())
-    date = db.Column('Created', db.DateTime, default=datetime.datetime.now)
-    description = db.Column('Description', db.Text())
-    skills = db.Column('Skills', db.String())
-    github = db.Column('GitHub Link', db.String())
-
-    def __repr__(self):
-        return f'''< New_Project (Title: {self.title}, Created: {self.date}, Description: {self.description},
-          Skills: {self.skills}, GitHub: {self.github})'''
 
 
 
